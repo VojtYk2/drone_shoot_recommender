9eca7df23215a9e3b30013bde1660cd7f82f026c
VojtYk2 <v.machacek.st@spseiostrava.cz>
2025-07-24 20:55:30 +0200

    Added more style, made the map bigger, added logic for choosing the spot on the map, started work on range selection slider

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,19 @@
 // Used the Leaflet tutorial on their website for help
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 10);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([51.505, -0.09]).addTo(map);
+var lastMarker = null;
+
+lastMarker = marker;
+
+function onMapClick(e) {
+    marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    map.removeLayer(lastMarker);
+    lastMarker = marker;
+}
+
+map.on('click', onMapClick);

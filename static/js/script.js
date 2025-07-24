afdae1226cc38e7b6beb700dd4ed9368a749c8f0
VojtYk2 <v.machacek.st@spseiostrava.cz>
2025-07-24 21:35:18 +0200

    Radius slider is now fully working including it changing the circle on the map

--- static/js/script.js
+++ static/js/script.js
@@ -14,6 +14,32 @@ function onMapClick(e) {
     marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
     map.removeLayer(lastMarker);
     lastMarker = marker;
+    createCircle();
 }
+
+var circle = L.circle([51.505, -0.09], {
+    radius: document.getElementById('radius').value,
+    color: 'blue',
+    fillColor: '#30f',
+    fillOpacity: 0.5
+}).addTo(map);
+var lastCircle = null;
+
+lastCircle = circle;
+
+function createCircle() {
+    circle = L.circle([marker.getLatLng().lat, marker.getLatLng().lng], {
+        radius: document.getElementById('radius').value,
+        color: 'blue',
+        fillColor: '#30f',
+        fillOpacity: 0.5
+    }).addTo(map);
+    map.removeLayer(lastCircle);
+    lastCircle = circle;
+}
+
+document.getElementById('radius').addEventListener('input', function() {
+    createCircle();
+});
 
 map.on('click', onMapClick);

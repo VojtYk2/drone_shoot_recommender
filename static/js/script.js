a7aa719757cb2236a9258faaec45e322a9aa4460
vojtyk_dell <vojtech.machacek1@gmail.com>
2025-08-14 12:08:02 +0200

    Resizing page fully working, added current location start point

--- static/js/script.js
+++ static/js/script.js
@@ -1,47 +1,82 @@
 // Used the Leaflet tutorial on their website for help
-var map = L.map('map').setView([51.505, -0.09], 10);
+var map = L.map('map');
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.505, -0.09]).addTo(map);
-var lastMarker = marker;
+var marker;
+var circle;
+
+function success(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    map.setView([lat, lng], 10);
+    marker = L.marker([lat, lng]).addTo(map);
+    circle = L.circle([lat, lng], {
+        radius: document.getElementById('radius').value,
+        color: 'blue',
+        fillColor: '#30f',
+        fillOpacity: 0.5
+    }).addTo(map);
+}
+
+function fail(error) {
+    map.setView([51.505, -0.09], 10);
+    marker = L.marker([51.505, -0.09]).addTo(map);
+    circle = L.circle([51.505, -0.09], {
+        radius: document.getElementById('radius').value,
+        color: 'blue',
+        fillColor: '#30f',
+        fillOpacity: 0.5
+    }).addTo(map);
+}
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, fail, {enableHighAccuracy: true, timeout: 5000, maximumAge: 0});
+}
+else {
+    map.setView([51.505, -0.09], 10);
+    marker = L.marker([51.505, -0.09]).addTo(map);
+    circle = L.circle([51.505, -0.09], {
+        radius: document.getElementById('radius').value,
+        color: 'blue',
+        fillColor: '#30f',
+        fillOpacity: 0.5
+    }).addTo(map);
+}
+
+let gen = false;
+let load = false;
 
 function onMapClick(e) {
+    if (marker) {
+        map.removeLayer(marker);
+    }
     marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-    map.removeLayer(lastMarker);
-    lastMarker = marker;
     createCircle();
 }
 
-var circle = L.circle([51.505, -0.09], {
-    radius: document.getElementById('radius').value,
-    color: 'blue',
-    fillColor: '#30f',
-    fillOpacity: 0.5
-}).addTo(map);
-var lastCircle = null;
-
-lastCircle = circle;
-
 function createCircle() {
+    if (circle) {
+        map.removeLayer(circle);
+    }
     circle = L.circle([marker.getLatLng().lat, marker.getLatLng().lng], {
         radius: document.getElementById('radius').value,
         color: 'blue',
         fillColor: '#30f',
         fillOpacity: 0.5
     }).addTo(map);
-    map.removeLayer(lastCircle);
-    lastCircle = circle;
 }
 
 function submitForm() {
     var lat = marker.getLatLng().lat;
     var lng = marker.getLatLng().lng;
     var radius = document.getElementById('radius').value;
+    gen = true;
+    load = true;
 
-    if(window.innerWidth <= 700) {
+    if (window.innerWidth <= 900) {
         document.getElementsByClassName('mapDiv')[0].style.display = 'none';
     }
 
@@ -79,28 +114,35 @@ function submitForm() {
 
 function displayNoResults(message) {
     const recommendationsDiv = document.getElementsByClassName('recommendationsDiv')[0];
-    recommendationsDiv.innerHTML = '<button class="generate-again">Generate Again</button><h2>Recommendations</h2>';
+    load = false;
+    if (window.innerWidth <= 900) {
+        recommendationsDiv.innerHTML = '<button class="generate-again">Generate Again</button><h2>Recommendations</h2>';
+        
+        document.getElementsByClassName('generate-again')[0].addEventListener('click', function() {
+            document.getElementsByClassName('mapDiv')[0].style.display = 'flex';
+            document.getElementsByClassName('recommendationsDiv')[0].innerHTML = '';
+        });
+    }
+    else {
+        recommendationsDiv.innerHTML = '<h2>Recommendations</h2>';
+    }
     const noResultsMessage = document.createElement('p');
     noResultsMessage.textContent = message;
     recommendationsDiv.appendChild(noResultsMessage);
-    
-    document.getElementsByClassName('generate-again')[0].addEventListener('click', function() {
-        document.getElementsByClassName('mapDiv')[0].style.display = 'flex';
-        document.getElementsByClassName('recommendationsDiv')[0].innerHTML = '';
-    });
 }
 
 function displayRecommendations(recommendations) {
     const recommendationsDiv = document.getElementsByClassName('recommendationsDiv')[0];
-    recommendationsDiv.innerHTML = '<button class="generate-again">Generate Again</button><h2>Recommendations</h2>';
+    load = false;
+    if (window.innerWidth <= 900) {
+        recommendationsDiv.innerHTML = '<button class="generate-again">Generate Again</button><h2>Recommendations</h2>';
+    }
+    else {
+        recommendationsDiv.innerHTML = '<h2>Recommendations</h2>';
+    }
     const recommendationsContainer = document.createElement('div');
     recommendationsContainer.className = 'recommendations';
     recommendationsDiv.appendChild(recommendationsContainer);
-
-    document.getElementsByClassName('generate-again')[0].addEventListener('click', function() {
-        document.getElementsByClassName('mapDiv')[0].style.display = 'flex';
-        document.getElementsByClassName('recommendationsDiv')[0].innerHTML = '';
-    });
 
     recommendations.forEach(element => {
         const a = document.createElement('a');
@@ -123,7 +165,35 @@ document.getElementById('radius').addEventListener('input', function() {
     createCircle();
 });
 
+
 document.getElementById('radius').value = 5000;
 document.getElementById('radiusOutput').value = document.getElementById('radius').value;
 
 map.on('click', onMapClick);
+
+window.addEventListener('resize', function() {
+    if(gen){
+        if (window.innerWidth <= 900) {
+            document.getElementsByClassName('mapDiv')[0].style.display = 'none';
+
+            if (!document.getElementsByClassName('generate-again')[0] && !load) {
+                var div = document.getElementsByClassName('recommendationsDiv')[0];
+                var inner = div.innerHTML;
+                div.innerHTML = '<button class="generate-again">Generate Again</button>' + inner;
+
+                document.getElementsByClassName('generate-again')[0].addEventListener('click', function() {
+                    document.getElementsByClassName('mapDiv')[0].style.display = 'flex';
+                    gen = false;
+                    document.getElementsByClassName('recommendationsDiv')[0].innerHTML = '';
+                });
+            }
+        }
+        else {
+            document.getElementsByClassName('mapDiv')[0].style.display = 'flex';
+
+            if (document.getElementsByClassName('generate-again')[0]) {
+                document.getElementsByClassName('generate-again')[0].remove();
+            }
+        }
+    }
+});
